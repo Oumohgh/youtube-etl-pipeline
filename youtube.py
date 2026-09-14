b15c1f8cf9cc73ad6a1efa8d291adbb0ff0e7a04
oughlane <mohammedoughlane72@gmail.com>
2026-09-14 14:31:39 +0100

    initialiser les bases donnes et creer une function pour fetcher api playlist

---
+++ youtube.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+API_KEY = "AIzaSyBOfQHV_9lPonsEZSSMRZrE0JXAXmoWWtQ"
+CHANNEL_HANDLE = "@Apprendrelefrench"
+BASE_URL = "https://www.googleapis.com/youtube/v3"
+
+def get_playlist_id(channel):
+    data= respon
+    
